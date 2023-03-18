30991fc7250dad043f8ddebf0065c6f073de95e8
coma-dwc <125862959+tamagomapon@users.noreply.github.com>
2023-03-18 14:34:36 +0000

    [finish]image_tag追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to users_path(@user.id)
+  end
+
 
   def index
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :books
   devise_for :users
 
-  resources :books, only: [:new, :index, :show, :edit]
+  resources :books, only: [:new, :index, :show, :edit, :create]
   resources :users, only: [:show, :edit, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
