275037c6a0668e776055bd978dcb130063a130b4
coma-dwc <125862959+tamagomapon@users.noreply.github.com>
2023-03-19 14:18:14 +0000

    [finish]0319作業終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,13 +6,19 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save!
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "You hane created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
     @book_new = Book.new
-    @book = Book.all
+    @books = Book.all
     @book = Book.new
   end
 
@@ -23,7 +29,22 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
   end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(params[:id])
+    else
+      render :edit
+    end
+
 
   def destroy
     @book = Book.find(params[:id])
