318b63f68ec1683c4446ff9706953e63ab782e59
coma-dwc <125862959+tamagomapon@users.noreply.github.com>
2023-03-19 08:50:58 +0000

    [add]User infoの編集ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,12 @@ class UsersController < ApplicationController
 
   private
 
-
+def is_matching_login_user
+  @user = User.find(params[:id])
+  if @user!=current_user
+    redirect_to user_path(@user.id)
+  end
+end
 
   def user_params
     params.require(:book).permit(:name, :profile_image, :introduction)
