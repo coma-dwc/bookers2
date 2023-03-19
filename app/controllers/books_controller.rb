b95323530d2fe08101316a32748512b1bd13129b
coma-dwc <125862959+tamagomapon@users.noreply.github.com>
2023-03-19 11:54:24 +0000

    [add]bookのindexページの左側の作成(User info)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,8 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
+    @book.save!
+    redirect_to book_path(@book.id)
   end
 
   def index
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,24 +11,32 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
-    @book_new = Book.new
+    @user = current_user
   end
 
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
-    @book_new = Book.new
+    @book = Book.new
   end
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+        render "edit"
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to users_path(@user.id)
+    if @user.update(user_params)
+       redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
 
@@ -42,7 +50,7 @@ def is_matching_login_user
 end
 
   def user_params
-    params.require(:book).permit(:name, :profile_image, :introduction)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
   def book_params
