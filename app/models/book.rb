58da5b6fb4f51cd0efe8056497bdfe5e1e4c2a26
coma-dwc <125862959+tamagomapon@users.noreply.github.com>
2023-03-20 11:55:19 +0000

    [add]部分テンプレートの作成・呼び出し

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,10 @@ class Book < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :body, length: { maximum: 200 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
