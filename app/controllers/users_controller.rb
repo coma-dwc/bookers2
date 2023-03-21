3b4f2dd92324e8f5c48da6673dd6615480362e7f
coma-dwc <125862959+tamagomapon@users.noreply.github.com>
2023-03-21 06:54:43 +0000

    [finish]RSpecテスト修正終了・完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
 def is_matching_login_user
   @user = User.find(params[:id])
   if @user!=current_user
-    redirect_to user_path(@user.id)
+    redirect_to user_path(current_user)
   end
 end
 
